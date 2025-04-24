6cc0bb38d12530c22075f872b9251dad379e9eb4
victoriaENA <victoria.notarianni.armijo@gmail.com.>
2025-04-24 13:36:53 +0100

    Wrote Main class to test program (I had already wrote it to test throughout but I forgot to commit

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // Create horses 
+        Horse horse1 = new Horse('♞', "Blaze", 0.3);
+        Horse horse2 = new Horse('♘', "Lightning", 0.6);
+        Horse horse3 = new Horse('♔', "Ray", 0.9);
+
+        Race race = new Race(20, 4);
+        race.addHorse(horse1, 1);
+        race.addHorse(horse2, 2);
+        race.addHorse(horse3, 3);
+
+        race.startRace(); // Run the race
 
     }
 }
